c4e8e4f4aec8eb410d7fcc8683e1de2bd5b33e99
dy40 <141488379+dy40@users.noreply.github.com>
2023-09-09 14:33:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import openai
+import time
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+# OPENAI API Key初始化設定
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def GPT_response(text):
+    # 接收回應
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    print(response)
+    # 重組回應
+    answer = response['choices'][0]['text'].replace('。','')
+    return answer
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    (start_year, start_month, end_year, end_month, stock_code):
+    start_date = str(date(start_year, start_month, 1))
+    end_date = str(date(end_year, end_month, 1))
+    month_list = pd.date_range(start_date, end_date, freq='MS').strftime("%Y%m%d").tolist()
+    
+    df = pd.DataFrame()
+    for month in month_list:
+        url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date="+ month + "&stockNo=" + str(stock_code)
+        res = r.get(url)
+        stock_json = res.json()
+        stock_df = pd.DataFrame.from_dict(stock_json['data'])
+        df=pd.concat([df,stock_df],ignore_index=True)
+        
+    # 資料轉型
+    for col in [0, 1, 2, 3, 4, 5, 6, 8]:
+        for row in range(df.shape[0]):
+            # 把"日期"從字串(string)換成時間(datetime)，並將民國年換成西元年
+            if col == 0:
+                day = df.iloc[row,0].split('/')
+                df.iloc[row, 0] = datetime(int(day[0]) + 1911, int(day[1]), int(day[2]))  
+            # 把"開盤價", "最高價", "最低價", "收盤價"帶有逗號的字串(string)換成浮點數(float) 
+            elif col != 0:
+                df.iloc[row, col] = float(df.iloc[row,col].replace(',', ''))
+    
+    df.columns = ['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']
+    return df
+
+stock_code_list = ['2330','2317','2454','2308','2402','2881','2382','6505','2882','2303']
+
+# 创建ExcelWriter对象，指定文件名
+with pd.ExcelWriter("stock_data.xlsx", engine="xlsxwriter") as writer:
+    for stock_code in stock_code_list:
+        stock_df = get_tw_stock_data(start_year=2023, 
+                                     start_month=1, 
+                                     end_year=2023, 
+                                     end_month=7, 
+                                     stock_code=stock_code)
+        
+        # 在每次迭代时，将对应股票代号的数据存储到相应的工作表中
+        stock_df.to_excel(writer, sheet_name=stock_code, index=False)
+
+        time.sleep(2)
+        
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
