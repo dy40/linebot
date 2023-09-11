0f44e401310de4d16613d4916b0f988e609fb357
dy40 <141488379+dy40@users.noreply.github.com>
2023-09-11 22:13:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import time
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    mtext = event.message.text
+    if mtext == '@股票':
+        try:
+            message = [
+                TextSendMessage( #傳文字
+                text = "yahoo股市"
+                ),
+                ImageSendMessage( #
+                    original_content_url="https://tw.stock.yahoo.com/"
+        
+                ),
+                TextSendMessage(  # 傳文字
+                    text="後面陸續增加"
+                )]
+      
+
+if __name__ == '__main__':
+    app.run()
